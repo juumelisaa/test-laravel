ce79d61a99db5cf6006d280cd273eb867a085e6b
juumelisaa <jumelisah@mcm.id>
2025-08-19 11:13:07 +0700

    add config database none

--- app/test-react/config/database.php
+++ app/test-react/config/database.php
@@ -112,12 +112,11 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-        'null' => [
+        'none' => [
             'driver' => 'sqlite',
             'database' => ':memory:',
             'prefix' => '',
         ],
-
     ],
 
     /*
