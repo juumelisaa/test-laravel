2c9d3c20910a8da97e3596ceeef3a49850906dbe
juumelisaa <jumelisah@mcm.id>
2025-08-20 11:05:48 +0700

    remove build folder in dockerfile before rebuild vite

--- app/app/resources/views/livewire/posts.blade.php
+++ app/app/resources/views/livewire/posts.blade.php
@@ -3,9 +3,9 @@
     @if ($isLoading)
         <p>LOADING</p>
     @endif
-    <ul class="grid grid-cols-5">
+    <ul class="grid grid-cols-5 bg-[#0a0a0a]">
     @foreach($posts as $post)
-        <li>{{ $post['title'] }}</li>
+        <li class="text-[#dd292f] ">{{ $post['title'] }}</li>
     @endforeach
     </ul>
 </div>
