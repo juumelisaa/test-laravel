3f0c93f75beab2846cef6e8b682d1214626c82d7
juumelisaa <jumelisah@mcm.id>
2025-08-20 11:45:04 +0700

    delete public folder before rebuild

--- app/app/vite.config.js
+++ app/app/vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/livewire',
+    base: process.env.BASE_URL || '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
