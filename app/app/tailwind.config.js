f7918d7683c3533e82ac3d022af56d6eaf632210
juumelisaa <jumelisah@mcm.id>
2025-08-20 09:53:16 +0700

    install tailwind

---
+++ app/app/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/views/components/**/*.blade.php', // include your layouts folder
+    './resources/js/**/*.vue',
+    './resources/js/**/*.jsx',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
