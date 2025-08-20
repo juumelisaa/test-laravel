20fc16ed17e1fc47ff21c593fe361984a952f9e0
juumelisaa <jumelisah@mcm.id>
2025-08-20 10:22:08 +0700

    import css in js file

--- app/app/vite.config.js
+++ app/app/vite.config.js
@@ -3,10 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: process.env.BASE_URL || '/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
